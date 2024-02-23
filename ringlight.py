16d81f9d261515f73a97f441c71ae0be7477188c
MikkelMohrHansen <147592404+MikkelMohrHansen@users.noreply.github.com>
2024-02-23 12:29:06 +0100

    update ringlight

--- ringlight.py
+++ ringlight.py
@@ -1,6 +1,15 @@
-from neopixel import NeoPixel 
-pin = Pin(0, Pin.OUT) # set GPIO0 to output to drive NeoPixels 
-np = NeoPixel(pin, 8) # create NeoPixel driver on GPIO0 for 8 pixels 
-np[0] = (255, 255, 255) # set the first pixel to white 
-np.write() # write data to all pixels 
-r, g, b = np[0] # get first pixel colour 
+from neopixel import NeoPixel
+from machine import Pin
+from time import sleep
+pin = Pin(7, Pin.OUT) # set GPIO0 to output to drive NeoPixels 
+np = NeoPixel(pin, 12) # create NeoPixel driver on GPIO0 for 8 pixels 
+ 
+
+while True:
+    for lights in range(0, 12):
+        np[lights - 1] = (0, 0, 0) # set the first pixel to white
+        np[lights] = (200, 0, 0) # set the first pixel to white
+        np.write() # write data to all pixels
+        sleep(0.0125)
+        
+        
