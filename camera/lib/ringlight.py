b3e339a42fc9c675fa8528da767fc6a20bfcc578
minityhyi <147266406+minityhyi@users.noreply.github.com>
2024-02-26 06:10:36 +0100

    davs

---
+++ camera/lib/ringlight.py
@@ -0,0 +1,31 @@
+from neopixel import NeoPixel
+from machine import Pin
+from time import sleep
+
+class NeoPx: 
+    def __init__(self, pin: int = 0):
+        self.pin = Pin(pin, Pin.OUT) 
+        self.np = NeoPixel(pin, 12)
+
+    def on(self):
+        for lights in range(0, 12): 
+            self.np[lights] = (255, 255, 255)
+            self.np.write()
+
+    def off(self):
+         for lights in range(0, 12): 
+            self.np[lights] = (0, 0, 0)
+            self.np.write()
+
+
+if __name__ == "__main__":
+    led = NeoPx(pin=7)
+    while True:
+        for lights in range(0, 12):
+                led.np[lights - 1] = (0, 0, 0)
+                led.np[lights] = (200, 0, 0)
+                led.np.write()
+                sleep(0.0125)
+        
+        
+        
