0938446da43ea38bbfc1d58e6c9251de29dd0d60
minityhyi <andreashc@live.dk>
2024-03-06 12:28:36 +0100

    slettet wifi

--- lib/sendimg.py
+++ lib/sendimg.py
@@ -3,44 +3,6 @@ import socket
 import sys
 import time
 
-# ConnectHandler behøves nok ikke i dette script, brugt til test... 
-class ConnectHandler:
-    def __init__(self, ssid, password):        
-        self.ssid = ssid
-        self.password = password
-
-    def activate(self):
-        sta_if = network.WLAN(network.STA_IF)
-
-        if not sta_if.isconnected():
-            sta_if.active(True)
-            
-            try:
-                sta_if.config(dhcp_hostname="ESP32")
-                sta_if.connect(self.ssid, self.password)
-            except Exception as err:
-                sta_if.active(False)
-                print("Error:", err)
-                sys.exit()
-            print("Connecting", end="")
-            n = 0
-            while not sta_if.isconnected():
-                print(".", end="")
-                time.sleep(1)
-                n += 1
-                if n == 200:
-                    break
-            if n == 200:
-                sta_if.active(False)
-                print("\nGiving up! Not connected!")
-                return ""
-            else:
-                print("\nNow connected with IP: ", sta_if.ifconfig()[0])
-                return sta_if.ifconfig()[0]
-        else:
-            print("Already Connected. ", sta_if.ifconfig()[0])
-            return sta_if.ifconfig()[0]
-
 class ImgFileSender:
     def __init__(self, serverIP, serverPort):
         self.serverAddress = (serverIP, serverPort)
